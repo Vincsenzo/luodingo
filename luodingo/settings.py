421a997a6be688123fc9aae1f516709e42c178ab
Vince Szabó <szabo.vince@icloud.com>
2025-04-02 11:08:45 +0200

    something

--- luodingo/settings.py
+++ luodingo/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
